df1dc230b96dd392ac0f1ac74b3c764d21f69372
illeandrei <ille.andrei.v@gmail.com>
2016-10-23 11:23:08 +0300

    -new layout

--- app/view/typing/Widget.js
+++ app/view/typing/Widget.js
@@ -5,7 +5,8 @@ Ext.define('TypinApp.view.typing.Widget',{
         'TypinApp.view.typing.WidgetController',
         'TypinApp.view.typing.WidgetModel',
         'Ext.ux.LiveSearchGridPanel',
-        'TypinApp.store.Versete'
+        'TypinApp.store.Versete',
+        'Ext.layout.container.Border'
     ],
 
     controller: 'typing-widget',
@@ -13,103 +14,104 @@ Ext.define('TypinApp.view.typing.Widget',{
         type: 'typing-widget'
     },
 
-    cls: 'widget',
+    // cls: 'widget',
+
+    layout: 'border',
+    width: 500,
+    height: 400,
+
+    bodyBorder: false,
+
+    defaults: {
+        collapsible: true,
+        bodyPadding: 10
+    },
 
     initComponent: function(){
         var me = this;
 
         Ext.apply(me, {
-            defaults: {
-                style: {
-                    marginRight: '10px',
-                    paddingLeft: '20px',
-                    paddingRight: '20px'
-                }
-            },
             items: [
-                me.comboForm = new Ext.form.Panel({
-                    layout: 'hbox',
-                    cls: 'comboForm',
+                {
+                    title: 'Typing App',
+                    region: 'north',
+                    height: 150,
+                    minHeight: 50,
+                    maxHeight: 100,
+                    items: [
+                        me.comboForm = new Ext.form.Panel({
+                            layout: 'hbox',
+                            cls: 'comboForm',
+                            defaults: {
+                                xtype: 'combobox',
+                                displayField: 'name',
+                                queryMode: 'local',
+                                labelWidth: 50,
+                                flex: 1
+                            },
+                            items: [
+                                {
+                                    fieldLabel: 'Book',
+                                    valueField: 'abbr',
+                                    padding: '0 20 0 0',
+                                    store: {
+                                        fields: ['abbr', 'name'],
+                                        data : [
+                                            {
+                                                "abbr":"1",
+                                                "name":"Geneza"
+                                            },
+                                            {
+                                                "abbr":"2",
+                                                "name":"Ioan"
+                                            },{
+                                                "abbr":"3",
+                                                "name":"Apocalipsa"
+                                            }
+                                        ]
+                                    }
+                                },
+                                {
+                                    fieldLabel: 'Chapter',
+                                    cls: 'chapter',
+                                    store: {
+                                        fields: ['name'],
+                                        data : [
+                                            {
+                                                "name":"1"
+                                            },
+                                            {
+                                                "name":"2"
+                                            },{
+                                                "name":"3"
+                                            }
+                                        ]
+                                    },
+                                    listeners: {
+                                        select: 'selectChapter'
+                                    }
+                                }
+                            ]
+                        })
+                    ]
+                },
+                {
+                    collapsible: false,
+                    region: 'center',
+                    layout: 'vbox',
                     defaults: {
-                        xtype: 'combobox',
-                        displayField: 'name',
-                        queryMode: 'local',
-                        labelWidth: 50
+                        width: '100%'
                     },
                     items: [
-                        {
-                            fieldLabel: 'Book',
-                            valueField: 'abbr',
-                            store: {
-                                fields: ['abbr', 'name'],
-                                data : [
-                                    {
-                                        "abbr":"1",
-                                        "name":"Geneza"
-                                    },
-                                    {
-                                        "abbr":"2",
-                                        "name":"Ioan"
-                                    },{
-                                        "abbr":"3",
-                                        "name":"Apocalipsa"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            fieldLabel: 'Chapter',
-                            cls: 'chapter',
-                            store: {
-                                fields: ['name'],
-                                data : [
-                                    {
-                                        "name":"1"
-                                    },
-                                    {
-                                        "name":"2"
-                                    },{
-                                        "name":"3"
-                                    }
-                                ]
-                            },
-                            listeners: {
-                                select: 'selectChapter'
-                            }
-                        }
+                        me.displayField = new Ext.form.field.Display({
+                            padding: '0 0 0 10'
+                        }),
+                        me.textarea = new Ext.form.field.TextArea({
+                            // cls: 'typing-area',
+                            disabled: true
+                        })
                     ]
-                }),
-                /*me.grid = Ext.create('Ext.ux.LiveSearchGridPanel', {
-                    store: Ext.create('TypinApp.store.Versete'),
-                    columnLines: true,
-                    columns: [
-                        {
-                            flex     : 1,
-                            dataIndex: 'verset'
-                        }
-                    ],
-                    height: 350,
-                    width: 800,
-                    viewConfig: {
-                        stripeRows: false
-                    }
-                })*/
-
-                me.displayField = new Ext.form.field.Display({
-                    xtype: 'displayfield'
-                    /*renderer: function (value, field) {
-                        return '<h2>' + value + '<h2>';
-                    }*/
-                    /*,
-                    fieldLabel: 'Text',
-                    bind: {
-                        value: '{name}'
-                    }*/
-                }),
-                me.textarea = new Ext.form.field.TextArea({
-                    cls: 'typing-area',
-                    width: '100%'
-                })
+                }
             ]
         });
 
@@ -118,7 +120,7 @@ Ext.define('TypinApp.view.typing.Widget',{
 
     initEvents: function () {
         var me = this;
-        
+
         me.textarea.on({
             change: 'isTyping'
         })
