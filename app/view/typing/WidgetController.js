37e8cc6e3835c5ec228a7d6e493423d362111bef
illeandrei <ille.andrei.v@gmail.com>
2016-11-06 15:32:44 +0200

    -rearrange methods for controller

--- app/view/typing/WidgetController.js
+++ app/view/typing/WidgetController.js
@@ -9,129 +9,6 @@ Ext.define('TypinApp.view.typing.WidgetController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.typing-widget',
 
-    isTyping: function (field , newValue) {
-        var me = this,
-            view = me.getView();
-
-        me.togglePanel(false);
-
-        //TODO: remove span if value is empty
-        /*if(newValue == ''){
-            console.warn('new value now: ', newValue);
-            me.resetValue();
-        }*/
-
-        if(index == 0){
-            originalValue = view.displayField.getValue();
-        } else {
-            me.resetValue();
-        }
-
-        if(originalValue.substring(0, newValue.length) == newValue.substring(0)){
-            index++;
-            correctValue = newValue;
-            me.changeColor(newValue, false);
-        } else {
-            errorCount++;
-            me.changeColor(newValue, true)
-        }
-
-        if(newValue.length == originalValue.length){
-            //TODO: trigger on some other condition
-            // me.showResults();
-            me.appendText();
-        }
-    },
-
-    appendText: function () {
-        var view = this.getView(),
-            activeValue = originalValue.concat(" " + verses[nextVerse]);
-
-        view.displayField.setValue(activeValue);
-        view.inactiveDisplay.setValue(verses[nextVerse + 1]);
-        nextVerse++;
-        originalValue = activeValue;
-    },
-
-    resetValue: function () {
-        var view = this.getView();
-        view.displayField.setValue(originalValue);
-    },
-
-    restartApp: function () {
-        var me           = this,
-            view         = me.getView(),
-            chapterCombo = Ext.getCmp('chapterCombo'),
-            textarea     = view.textarea;
-
-        //TODO: see if my 'originalValue' can be replaced
-        view.resultWindow.close();
-        textarea.setValue(textarea.originalValue);
-        me.resetValue();
-        me.togglePanel(true);
-        chapterCombo.focus(null, 500);
-    },
-
-    wordsMinute: function () {
-        return originalValue
-            .split(/(\s+)/)
-            .filter(function (word) {
-                return word != ' ';
-            })
-            .length;
-    },
-
-    showResults: function () {
-        var me = this,
-            view = this.getView();
-
-        // var wordCount = me.wordsMinute();
-
-        //TODO: restart app on Enter
-        //TODO: add results to result screen
-        /*view.resultWindow.add({
-            xtype: 'gauge',
-            flex: 1,
-            value: errorCount
-        });*/
-        view.resultWindow.show();
-    },
-
-    changeColor: function (newValue, mistyped) {
-        var view = this.getView(),
-            valueLength = newValue.length;
-
-        if(mistyped){
-            view.displayField.setValue(
-                    '<span class="change-green">' +
-                        originalValue.substring(0, correctValue.length) +
-                    '</span>' +
-                    '<span class="change-red">' +
-                        originalValue.substring(correctValue.length) +
-                    '</span>'
-            );
-        } else {
-            view.displayField.setValue(
-                    '<span class="change-green">' +
-                        originalValue.substring(0, valueLength) +
-                    '</span>' +
-                        originalValue.substring(valueLength)
-            );
-        }
-
-    },
-
-    togglePanel: function (show) {
-        var me = this.getView(),
-            panel = Ext.getCmp('northPanel');
-
-        if(show){
-            panel.expand();
-        } else {
-            panel.collapse();
-        }
-    },
-
     selectBook: function (combo, record) {
         var me = this,
             view = me.getView(),
@@ -211,6 +88,122 @@ Ext.define('TypinApp.view.typing.WidgetController', {
             str = str.replace(new RegExp(i, "g"), convMap[i]);
         }
         return str;
+    },
+
+    isTyping: function (field , newValue) {
+        var me = this,
+            view = me.getView();
+
+        me.togglePanel(false);
+
+        //TODO: remove span if value is empty
+        /*if(newValue == ''){
+            console.warn('new value now: ', newValue);
+            me.resetValue();
+        }*/
+
+        // if(index == 0){
+        //     originalValue = view.displayField.getValue();
+        // } else {
+        //     me.resetValue();
+        // }
+
+        if(originalValue.substring(0, newValue.length) == newValue.substring(0)){
+            index++;
+            correctValue = newValue;
+            me.changeColor(newValue, false);
+        } else {
+            errorCount++;
+            me.changeColor(newValue, true)
+        }
+
+        if(newValue.length == originalValue.length){
+            //TODO: trigger on some other condition
+            // me.showResults();
+            me.appendText();
+        }
+    },
+
+    changeColor: function (newValue, mistyped) {
+        var view = this.getView(),
+            valueLength = newValue.length;
+
+        if(mistyped){
+            view.displayField.setValue(
+                '<span class="change-green">' +
+                originalValue.substring(0, correctValue.length) +
+                '</span>' +
+                '<span class="change-red">' +
+                originalValue.substring(correctValue.length) +
+                '</span>'
+            );
+        } else {
+            view.displayField.setValue(
+                '<span class="change-green">' +
+                originalValue.substring(0, valueLength) +
+                '</span>' +
+                originalValue.substring(valueLength)
+            );
+        }
+
+    },
+
+    appendText: function () {
+        var view = this.getView(),
+            activeValue = originalValue.concat(" " + verses[nextVerse]);
+
+        view.displayField.setValue(activeValue);
+        view.inactiveDisplay.setValue(verses[nextVerse + 1]);
+        nextVerse++;
+        originalValue = activeValue;
+    },
+
+    resetValue: function () {
+        var view = this.getView();
+        view.displayField.setValue(originalValue);
+    },
+
+    restartApp: function () {
+        var me           = this,
+            view         = me.getView(),
+            chapterCombo = Ext.getCmp('chapterCombo'),
+            textarea     = view.textarea;
+
+        //TODO: see if my 'originalValue' can be replaced
+        view.resultWindow.close();
+        textarea.setValue(textarea.originalValue);
+        me.resetValue();
+        me.togglePanel(true);
+        chapterCombo.focus(null, 500);
+    },
+
+    wordsMinute: function () {
+        return originalValue
+            .split(/(\s+)/)
+            .filter(function (word) {
+                return word != ' ';
+            })
+            .length;
+    },
+
+    showResults: function () {
+        var me = this,
+            view = this.getView();
+
+        // var wordCount = me.wordsMinute();
+
+        view.resultWindow.show();
+    },
+
+    togglePanel: function (show) {
+        var me = this.getView(),
+            panel = Ext.getCmp('northPanel');
+
+        if(show){
+            panel.expand();
+        } else {
+            panel.collapse();
+        }
     }
     
 });
